ef13810abbeb04475c3eb307ea4b92ac85183945
jwierzbicki <jwierzbicki22@wp.pl>
2018-05-06 09:34:17 +0200

    Added 'product add' and 'product delete' options

---
+++ src/main/resources/static/js/productadd.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$('#addButton').click(function() {
+
+		let formData = {};
+		let name = document.getElementById("name").value;
+		let category = document.getElementById("category").value;
+
+		formData.name = name;
+		formData.category = category;
+
+		$.ajax({
+			type : 'POST',
+			url : 'http://localhost:8080/products/add',
+			data : JSON.stringify(formData),
+			contentType : 'application/json; charset=utf-8',
+			dataType : 'json',
+			success : function(data) {
+				$('#name').val('');
+				$('#category').val('');
+				alert('Product added');
+			},
+			failure : function(errMsg) {
+				alert(errMsg);
+			}
+		});
+	});
+
+});
--- src/main/resources/static/js/productlist.js
+++ src/main/resources/static/js/productlist.js
@@ -1,30 +1,60 @@
 $(document).ready(function() {
-	
-	$.ajax({
-		url: "http://localhost:8080/products/all"
-	}).then(function(products) {
-		products.forEach(function(prod) {
-			
-			let row = document.createElement('tr');
-			
-			let id = document.createElement('td');
-			let idInner = document.createTextNode(prod.id);
-			id.appendChild(idInner);
-			
-			let name = document.createElement('td');
-			let nameInner = document.createTextNode(prod.name);
-			name.appendChild(nameInner);
-			
-			let cat = document.createElement('td');
-			let catInner = document.createTextNode(prod.category);
-			cat.appendChild(catInner);
-			
-			row.appendChild(id);
-			row.appendChild(name);
-			row.appendChild(cat);
-			
-			$('#productTable').append(row);
-		})
-	});
-});
 
+	refresh();
+	
+	$('#refresh').click(function() {
+		$('#tableBody').empty();
+		refresh();
+	})
+
+	function refresh() {
+
+		$.ajax({
+			url : "http://localhost:8080/products/all"
+		}).then(function(products) {
+			products.forEach(function(prod) {
+
+				let row = document.createElement('tr');
+
+				let id = document.createElement('td');
+				let idInner = document.createTextNode(prod.id);
+				id.appendChild(idInner);
+
+				let name = document.createElement('td');
+				let nameInner = document.createTextNode(prod.name);
+				name.appendChild(nameInner);
+
+				let cat = document.createElement('td');
+				let catInner = document.createTextNode(prod.category);
+				cat.appendChild(catInner);
+				
+				let del = document.createElement('td');
+				let delButton = document.createElement('input');
+				delButton.type = 'button';
+				delButton.value = 'Delete';
+				delButton.setAttribute('class', 'btn btn-danger');
+				delButton.onclick = function() {
+					$.ajax({
+						type : 'DELETE',
+						url : 'http://localhost:8080/products/' + prod.id,
+						success : function(data) {
+							alert('Product deleted. Click Refresh.');
+						},
+						failure : function(errMsg) {
+							alert(errMsg);
+						}
+					});
+				};
+				del.appendChild(delButton);
+
+				row.appendChild(id);
+				row.appendChild(name);
+				row.appendChild(cat);
+				row.appendChild(del);
+
+				$('#tableBody').append(row);
+			});
+		});
+	}
+
+});
